6146c59a4ec293c44fae45c3cae8a7fa7925985a
Gabriel França <gabrieldefran@hotmail.com>
2026-01-16 13:21:17 -0300

    Aula sobre pacote, comentários, convenções de variáveis,declaração e tamanho de memória e casting

---
+++ Java/DevDojo-Java-Virado-no-Juraya/maratona-java/src/com/gabrielfranca/maratonajava/introducao/Aula01OlaDevDojo.java
@@ -0,0 +1,16 @@
+package com.gabrielfranca.maratonajava.introducao;
+
+/**
+ * Isso é um comentário <b>Javadoc</b>
+ */
+public class Aula01OlaDevDojo {
+    //Isso é um comentário de uma linha
+    /*
+    Comentário de
+    multiplas
+    linhas
+     */
+    public static void main(String[] args){
+        System.out.println("KA-ME-HA-ME-HA");
+    }
+}
---
+++ Java/DevDojo-Java-Virado-no-Juraya/maratona-java/src/com/gabrielfranca/maratonajava/introducao/Aula02TiposPrimitovos.java
@@ -0,0 +1,18 @@
+package com.gabrielfranca.maratonajava.introducao;
+
+public class Aula02TiposPrimitovos {
+    public static void main(String[] args) {
+        // int, double, flooat, char, byte, short, long, boolean
+        int age = 10;
+        long numeroGrande = 10000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'M';
+        System.out.println("A idade é "+age+" anos");
+        System.out.println(verdadeiro);
+    }
+}
