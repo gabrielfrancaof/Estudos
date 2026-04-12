f4d56eef9c2cacb9efd531479305d69e68c558ac
Gabriel de França <gabrieldefran@hotmail.com>
2026-04-11 23:57:14 -0300

    Sequencia de fibonacci
    Aqui coloco a formula de fibonacci e uma explicação de como ela funciona de uma maneira que eu consegui entender

---
+++ faculdade/python/Programação/sequencia_fibonacci.py
@@ -0,0 +1,33 @@
+#sequencia fibonacci
+
+n = int (input("Digite a quantidade da sequencia: ")) #[5]
+a = 0
+b = 1
+
+print (a, end = " ")
+print (b, end = " ")
+for fibonacci in range (n): # [0, 1, 2, 3, 4]
+    c = a + b               #primeiro loop irá colocar c = 0 + 1
+    print (c, end = " ")   #print 1
+    a = b                   # 0 -> 1
+    b = c                   # 1 -> 1 
+
+
+'''
+#segunda sequencia será: 
+
+    c = a + b               # segundo loop irá colocar c = 1 + 1 
+    print (c, end = " ")    # print 2
+    a = b                   # 1 -> 1
+    b = c                   # 1 -> 2
+
+#terceira sequencia será:
+    c = a + b               # terceiro loop irá colocar c = 1 + 2
+    print (c, end = " ")    # print 3
+    a = b                   # 1 -> 2
+    b = c                   # 2 -> 3 
+
+
+
+
+'''
