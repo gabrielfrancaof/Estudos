ee3970917c6854374855282d9b2479a6d0d26b03
Gabriel França <gabrieldefran@hotmail.com>
2026-04-13 23:57:10 -0300

    Adição do excercício da aula 03

---
+++ faculdade/semestre-01/programacao-de-computadores/exercicios/aula-03-operadores/exerc02.py
@@ -0,0 +1,6 @@
+#Crie um programa que solicite o valor em horas para o usuario, calcule e mostre o valor em minutos
+
+valor_em_hora = int(input("Digite o valor de horas que deseja converter em minutos: "))
+calculo = valor_em_hora*60
+
+print (valor_em_hora,"h são", calculo,"minutos")
