5ccbcc43c752343da380d627265ad4ff6e77388d
Gabriel de França <gabrieldefran@hotmail.com>
2026-04-17 20:48:44 -0300

    Exercicio para imprimir os números de 1 a 50 de 1 em 1 e de 52 a 100 de 2 em 2

---
+++ faculdade/semestre-01/programacao-de-computadores/exercicios/aula-06-estruturas-de-repeticao/exerc02.py
@@ -0,0 +1,7 @@
+#Faça um programa em Python que imprima os números de 1 a 50 de 1 em 1 e de 52 a 100 de 2 em 2
+
+for i in range (1,51):
+    print (i,", ",end="") 
+
+for n in range (52, 101,2):
+    print (n, ", ", end="")
