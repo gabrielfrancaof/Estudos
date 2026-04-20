253e84a2afa9f8718154e6c4ab51f167a5e569ca
Gabriel França <gabrieldefran@hotmail.com>
2026-04-19 22:35:04 -0300

    Exercício para Criar   um   algoritmo   que   leia   a   idade   de   uma   pessoa   e   informe   sua   classe   eleitoral

---
+++ faculdade/semestre-01/programacao-de-computadores/exercicios/aula-05-if-aninhados/exerc01.py
@@ -0,0 +1,13 @@
+#Criar   um   algoritmo   que   leia   a   idade   de   uma   pessoa   e   informe   sua   classe   eleitoral :  
+# •   não - eleitor   (abaixo   de   16   anos)  
+# •   eleitor   obrigatório   (entre   18   e   65   anos)  
+# •   eleitor   facultativo   (entre   16   e   18   anos   e   maior   de   65   anos)
+
+idade = int(input("Digite a idade: "))
+
+if idade < 16:
+    print("Não eleitoral!")
+elif idade >= 16 and idade <18 or idade > 65:
+    print ("Eleitor facultativo!")
+else:
+    print("Eleitor obrigatório!")
