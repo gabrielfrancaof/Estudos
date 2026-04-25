d692b4179d1538f0827d8cf710232f6b154008f1
Gabriel de França <gabrieldefran@hotmail.com>
2026-04-24 21:43:23 -0300

    Exercício para fazer um método com retorno que receba como parâmetros os lados de um retângulo, calcula e retorna o valor de sua área

---
+++ faculdade/semestre-01/programacao-de-computadores/exercicios/aula-07-metodos/exerc02.py
@@ -0,0 +1,10 @@
+#Escreva um método com retorno que receba como parâmetros os lados de um retângulo, calcula e retorna o valor de sua área. area = lado*lado Faça um programa principal que solicite os valores dos lados de um retângulo ao usuário, e utilizando a função definida acima, calcule e mostre o valor de área
+
+
+import metodo02
+ladoa = int(input("Digite o lado a: "))
+ladob = int(input("Digite o lado b: "))
+
+print ("-"*30)
+metodo02.calculo(ladoa, ladob)
+print ("-"*30)
---
+++ faculdade/semestre-01/programacao-de-computadores/exercicios/aula-07-metodos/metodo02.py
@@ -0,0 +1,3 @@
+def calculo (ladoa,ladob):
+    area = ladoa*ladob
+    print ("A área é: ", area)
